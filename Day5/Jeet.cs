00d05a87aa22dfec3a89bf727ce5706b2004cc1f
nikhilgode <158138219+nikhilgode@users.noreply.github.com>
2024-02-03 18:39:12 +0530

    Add files via upload
    Started with EF

---
+++ Day5/Jeet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5
+{
+    internal class Jeet
+    {
+
+        public static int add(int x)
+        {
+            return x;
+        }
+
+        public static int add(String x)
+        {
+            string S = "FFSFS";
+            return 0; ;   
+
+        }
+    }
+}
